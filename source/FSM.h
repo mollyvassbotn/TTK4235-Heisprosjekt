bddb46623515deec0f3bad3a9c1a66170e8eea4b
Jananni Johanraj <janannij@stud.ntnu.no>
2020-02-25 11:34:27 +0100

    Dag 7

--- source/FSM.h
+++ source/FSM.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Finite state machine. Chooses the next state for the elevator.
+ */
+
 #ifndef FSM_H
 #define FSM_H
 
@@ -23,22 +28,65 @@ system_state g_current_state;
 
 system_state g_prev_state;
 
+/*
 void fsm_open_door();
 
 void fsm_close_door();
+*/
 
-void fsm_idle();
 
+/**
+ * @brief Defines a state for the elevator. Updates current floor.
+ */
 void init_fsm();
 
+
+/**
+ * @brief Checks if there are any orders, and sets current state.
+ */
+void fsm_idle();
+
+
+/**
+ * @brief Sets the elevator direction downwards, and stops at the ordered floors in
+ * the same direction. The orders will be deleted after they are done.
+ */
 void fsm_moving_down();
 
+
+/**
+ * @brief Sets the elevator direction upwards, and stops at the ordered floors in
+ * the same direction. The orders will be deleted after they are done.
+ */
 void fsm_moving_up();
 
+
+/**
+ * @brief Opens the door, and keeps it open for 3 seconds. If the obstuction signal
+ * is active, the door will open as long as it is active. Then it will close after
+ * seconds.
+ */
 void fsm_doors_open();
 
+
+/**
+ * @brief Stops the elevator immidiately. If the stop button is pushed, all orders
+ * will be deleted. If the elevator is at a floor as the stop button is pushed, the
+ * doors will open.
+ */
+void fsm_stop();
+
+
+/**
+ * @brief Queue system for the elevator. Priorities the orders, based on the
+ * previous direction.
+ */
 void next_order();
 
+
+/**
+ * @brief Updates orders and current floor. Sets the elevator state.
+ */
 void run_fsm();
 
 
